370b22880944db03af91b8967ab26a7fc1c8cff5
DylanRhinehart <dylanrhinehart@gmail.com>
2022-11-20 14:52:46 -0800

    HTML & CSS

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
+const button = document.getElementsByTagName(button);
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
